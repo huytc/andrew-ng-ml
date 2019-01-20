cf3305bb1edba58aa79b23084463bb26d265406d
huytc <1611331@hcmut.edu.vn>
2019-01-20 21:46:39 +0700

    implement gradient descent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,10 +17,10 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    theta = theta - alpha / m * X' * (X * theta - y); 
 
 
-
-
+    
 
 
     % ============================================================
