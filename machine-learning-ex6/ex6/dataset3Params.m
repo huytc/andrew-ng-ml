8acce21cb86889ff384fe8336bb68e7c6b2fb4d0
Huy Tran <tc.huy.7@gmail.com>
2019-01-30 15:37:02 +0700

    select best C and sigma

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_vec = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_vec = [0.01 0.03 0.1 0.3 1 3 10 30];
 
+error_val = zeros(length(C_vec), length(sigma_vec));
 
+for i = 1:length(C_vec)
+  for j = 1:length(sigma_vec)
+    model = svmTrain(X, y, C_vec(i), @(x1,x2)gaussianKernel(x1,x2,sigma_vec(j)));
+    predictions = svmPredict(model, Xval);
+    error_val(i,j) = mean(double(predictions ~= yval));
+  endfor
+endfor
 
+[min_error, min_row] = min(min(error_val, [], 2));
+[min_error, min_col] = min(min(error_val, [], 1));
 
-
+C = C_vec(min_row);
+sigma = sigma_vec(min_col);
 
 % =========================================================================
 
